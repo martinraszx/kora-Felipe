8a20f2ac3453f805bed404feb44894eae7218727
martinraszx <martinraszx@gmail.com>
2026-06-08 23:54:08 -0400

    update

--- src/screens/NosotrosScreen.js
+++ src/screens/NosotrosScreen.js
@@ -33,3 +33,5 @@ const styles = StyleSheet.create({
   avatarPlaceholder: { width: 75, height: 75, backgroundColor: '#E0E0E0', justifyContent: 'center', alignItems: 'center', borderWidth: 1, borderColor: '#000' },
   avatarX: { fontSize: 24, fontWeight: 'bold', color: '#555' }
 });
+
+//ola
